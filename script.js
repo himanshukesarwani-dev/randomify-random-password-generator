31b53a327776b3ac1e78cd25a7c8ecb3fd8e62c0
Himanshu <himanshukesarwani800@gmail.com>
2023-02-17 12:34:48 +0530

    refactor: changed const to let for pass1 & pass2

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const lengthOfPassword = document.querySelector("#length-of-password");
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"
 ];
-const passwordOne = document.getElementById("password-one");
-const passwordTwo = document.getElementById("password-two");
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
 let pass = "";
 
 
